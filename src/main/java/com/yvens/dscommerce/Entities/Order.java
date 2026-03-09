e95a05a40ecbbf3409b4c351510c28da520c5923
Yvens <98363275+YvensBonfim@users.noreply.github.com>
2026-03-09 10:06:00 -0300

    Equals Hashcode

--- src/main/java/com/yvens/dscommerce/Entities/Order.java
+++ src/main/java/com/yvens/dscommerce/Entities/Order.java
@@ -2,6 +2,7 @@ package com.yvens.dscommerce.Entities;
 
 import java.time.Instant;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import jakarta.persistence.CascadeType;
@@ -81,6 +82,48 @@ public class Order {
     public void setClient(User client) {
         this.client = client;
     }
+
+    public Payment getPayment() {
+        return payment;
+    }
+
+    public Set<OrderItem> getItems() {
+        return items;
+    }
+
+    
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Order other = (Order) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+
+    public List<Product> getProducts() {
+return items.stream().map(x -> x.getproduct()).toList();
+
+    }
+
+
 
 
 
--- src/main/java/com/yvens/dscommerce/Entities/Product.java
+++ src/main/java/com/yvens/dscommerce/Entities/Product.java
@@ -1,6 +1,7 @@
 package com.yvens.dscommerce.Entities;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import jakarta.persistence.Column;
@@ -86,8 +87,44 @@ public class Product {
         this.imgUrl = imgUrl;
     }
 
-    public Set<Category> getCategories() {
+    
+
+   public Set<Category> getCategories() {
         return categories;
     }
+
+    public Set<OrderItem> getItems() {
+        return items;
+    }
+
+    
+
+   @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Product other = (Product) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+
+   public List<Order> getOrders() {return items.stream().map(x -> x.getOrder()).toList();}
+
 
 }
